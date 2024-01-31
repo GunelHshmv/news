e25a752394f62b0c699b10ce7d1426e80c3f296f
GunelHashimova <114391396+GunelHshmv@users.noreply.github.com>
2024-01-31 23:50:57 +0400

    Update index.js

--- src/Routing/index.js
+++ src/Routing/index.js
@@ -2,19 +2,28 @@ import {   Navigate, Route, Routes } from 'react-router-dom';
 import CategorialNews from '../Pages/CategorialNews';
 import DetailedNews from '../Pages/DetailedNews';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 const Routing = () => {
+//     useEffect(() => {
+//         // const currentPath = window.location.pathname;
+
+//         // if (currentPath === "/news/") {
+//         //     window.location.replace("/"); 
+//         // }
+//         if (window.location.pathname.includes('/news/')) {
+//     const newUrl = window.location.href.replace('/news/', '/all');
+//     window.location.href = newUrl;
+// }
+
+//     }, []);
     useEffect(() => {
-        // const currentPath = window.location.pathname;
-
-        // if (currentPath === "/news/") {
-        //     window.location.replace("/"); 
-        // }
         if (window.location.pathname.includes('/news/')) {
-    const newUrl = window.location.href.replace('/news/', '/all');
-    window.location.href = newUrl;
-}
+            navigate('/all');
+        }
+    }, [navigate]);
 
-    }, []);
+    return null;
+};
     return <div>
         <Routes>
             <Route path='/all' exact element={<CategorialNews />} />
