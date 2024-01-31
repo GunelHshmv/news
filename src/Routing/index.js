4b90240e1c2745e2ec4381bb0401f1b5dbb1f88d
GunelHashimova <114391396+GunelHshmv@users.noreply.github.com>
2024-01-31 23:45:30 +0400

    Update index.js

--- src/Routing/index.js
+++ src/Routing/index.js
@@ -3,19 +3,19 @@ import CategorialNews from '../Pages/CategorialNews';
 import DetailedNews from '../Pages/DetailedNews';
 import { useEffect } from 'react';
 const Routing = () => {
-    // useEffect(() => {
-    //     const currentPath = window.location.pathname;
+    useEffect(() => {
+        const currentPath = window.location.pathname;
 
-    //     if (currentPath === "/news") {
-    //         window.location.replace("/"); 
-    //     }
-    // }, []);
+        if (currentPath === "/news/") {
+            window.location.replace("/"); 
+        }
+    }, []);
     return <div>
         <Routes>
             <Route path='/' exact element={<CategorialNews />} />
             <Route path='/:title' element={<CategorialNews/>}/>
             <Route path='/:title/:index' element={<DetailedNews/>} />
-            <Route path='/news/' element={<Navigate to="/all" />} />
+            // <Route path='/news/' element={<Navigate to="/all" />} />
         </Routes>
     </div>;
 }
