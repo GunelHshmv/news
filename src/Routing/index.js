75a4711585d16059ae73c2e79c56c6357433cf93
GunelHashimova <114391396+GunelHshmv@users.noreply.github.com>
2024-02-05 18:10:54 +0400

    Update index.js

--- src/Routing/index.js
+++ src/Routing/index.js
@@ -1,17 +1,25 @@
 import React, { useEffect } from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom'; // Navigate ekledik
+import { Route, Routes, Navigate } from 'react-router-dom';
 import CategorialNews from '../Pages/CategorialNews';
 import DetailedNews from '../Pages/DetailedNews';
-import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const Routing = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.pathname === '/news/') {
+            navigate('/all');
+        }
+    }, [location]);
+
     return (
         <div>
             <Routes>
                 <Route path='/all' element={<CategorialNews />} />
-                <Route path='/:title' element={<CategorialNews/>} />
-                <Route path='/:title/:index' element={<DetailedNews/>} />
-                {<Route path='/news/' element={<Navigate to="/all" />} /> 
+                <Route path='/:title' element={<CategorialNews />} />
+                <Route path='/:title/:index' element={<DetailedNews />} />
+                <Route path='/news/' element={<Navigate to="/all" />} />
             </Routes>
         </div>
     );
