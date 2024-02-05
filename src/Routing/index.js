5a6d6804a2c95c206c4723e5499a2581373a25b2
GunelHashimova <114391396+GunelHshmv@users.noreply.github.com>
2024-02-05 19:36:12 +0400

    setup gh-pages

--- src/Routing/index.js
+++ src/Routing/index.js
@@ -1,61 +1,25 @@
-// import React, { useEffect } from 'react';
-// import { Route, Routes, Navigate } from 'react-router-dom'; // Navigate ekledik
-// import CategorialNews from '../Pages/CategorialNews';
-// import DetailedNews from '../Pages/DetailedNews';
-// import { useNavigate } from 'react-router-dom';
-
-// const Routing = () => {
-//     const navigate = useNavigate(); // useNavigate hook'unu burada kullanın
-
-//     // useEffect(() => {
-//     //     if (window.location.pathname.includes('/news')) {
-//     //         navigate('/all');
-//     //     }
-//     // }, [navigate]);
-
-//     return (
-//         <div>
-//             <Routes>
-//                 <Route path='/all' element={<CategorialNews />} />
-//                 <Route path='/:title' element={<CategorialNews/>} />
-//                 <Route path='/:title/:index' element={<DetailedNews/>} />
-//                 <Route path='/news/' element={<Navigate to="/all" />} />
-//             </Routes>
-//         </div>
-//     );
-// }
-
-// export default Routing;
-
-
 import React, { useEffect } from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom'; // Navigate ekledik
 import CategorialNews from '../Pages/CategorialNews';
 import DetailedNews from '../Pages/DetailedNews';
 import { useNavigate } from 'react-router-dom';
 
 const Routing = () => {
-    const navigate = useNavigate();
+    const navigate = useNavigate(); // useNavigate hook'unu burada kullanın
 
     useEffect(() => {
-        const storedPath = localStorage.getItem('redirectPath');
-        
-        if (storedPath && window.location.pathname.includes('/news')) {
-            navigate(storedPath);
+        if (window.location.pathname.includes('/news/')) {
+            navigate('/all');
         }
     }, [navigate]);
 
     return (
         <div>
             <Routes>
-                {/* Ana sayfaya giden yol */}
-                <Route path='/' element={<CategorialNews />} />
-                {/* Tüm haberler için yol */}
                 <Route path='/all' element={<CategorialNews />} />
-                {/* Detaylı haberler için yol */}
-                <Route path='/:title/:index' element={<DetailedNews />} />
-                {/* Haberler için genel yol, burada Navigate yerine Redirect kullanılabilir */}
-                <Route path='/news/*' element={<Navigate to="/all" />} />
+                <Route path='/:title' element={<CategorialNews/>} />
+                <Route path='/:title/:index' element={<DetailedNews/>} />
+                <Route path='/news/' element={<Navigate to="/all" />} />
             </Routes>
         </div>
     );
