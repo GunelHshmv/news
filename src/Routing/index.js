44128a25beaf202577c92c54382df57113096b0e
GunelHashimova <114391396+GunelHshmv@users.noreply.github.com>
2024-02-05 18:55:36 +0400

    setup gh-pages

--- src/Routing/index.js
+++ src/Routing/index.js
@@ -5,10 +5,22 @@ import DetailedNews from '../Pages/DetailedNews';
 import { useLocation } from 'react-router-dom';
 
 const Routing = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        // Sayfa yüklendiğinde veya yenilendiğinde bu etkileşim gerçekleşir
+        // Eğer /news rotası açıldığında /all rotasına yönlendirme yap
+        if (location.pathname.startsWith('/news')) {
+            Navigate('/all');
+        }
+    }, [location]);
+
     return (
         <div>
             <Routes>
-            <Route path='/news/*' element={<Navigate to="/all" replace />} />
+                {/* exact özelliği kullanarak yalnızca /news rotası tam olarak eşleştiğinde Navigate yapılacak */}
+                <Route exact path='/news' element={<Navigate to="/all" replace />} />
+                <Route path='/all' element={<CategorialNews />} />
                 <Route path='/:title' element={<CategorialNews />} />
                 <Route path='/:title/:index' element={<DetailedNews />} />
             </Routes>
