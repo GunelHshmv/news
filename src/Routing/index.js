e01348fc8d80ec07980a6665e578ac787b293779
GunelHashimova <114391396+GunelHshmv@users.noreply.github.com>
2024-02-05 19:26:34 +0400

    setup gh-pages

--- src/Routing/index.js
+++ src/Routing/index.js
@@ -1,26 +1,25 @@
 import React, { useEffect } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom'; // Navigate ekledik
 import CategorialNews from '../Pages/CategorialNews';
 import DetailedNews from '../Pages/DetailedNews';
-import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Routing = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
+    const navigate = useNavigate(); // useNavigate hook'unu burada kullanın
 
-    useEffect(() => {
-        // Eğer /news rotası açıldığında /all rotasına yönlendirme yap
-        if (location.pathname.includes('/news/')) {
-            navigate('/all');
-        }
-    }, [location, navigate]);
+    // useEffect(() => {
+    //     if (window.location.pathname.includes('/news')) {
+    //         navigate('/all');
+    //     }
+    // }, [navigate]);
 
     return (
         <div>
             <Routes>
                 <Route path='/all' element={<CategorialNews />} />
-                <Route path='/:title' element={<CategorialNews />} />
-                <Route path='/:title/:index' element={<DetailedNews />} />
+                <Route path='/:title' element={<CategorialNews/>} />
+                <Route path='/:title/:index' element={<DetailedNews/>} />
+                <Route path='/news/' element={<Navigate to="/all" />} />
             </Routes>
         </div>
     );
