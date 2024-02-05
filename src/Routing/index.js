7a0230d88c9c22ff543d1ab074a2e4f01e0288d4
GunelHashimova <114391396+GunelHshmv@users.noreply.github.com>
2024-02-05 19:33:39 +0400

    setup gh-pages

--- src/Routing/index.js
+++ src/Routing/index.js
@@ -1,9 +1,38 @@
 // import React, { useEffect } from 'react';
+// import { Route, Routes, Navigate } from 'react-router-dom'; // Navigate ekledik
+// import CategorialNews from '../Pages/CategorialNews';
+// import DetailedNews from '../Pages/DetailedNews';
+// import { useNavigate } from 'react-router-dom';
+
+// const Routing = () => {
+//     const navigate = useNavigate(); // useNavigate hook'unu burada kullanın
+
+//     // useEffect(() => {
+//     //     if (window.location.pathname.includes('/news')) {
+//     //         navigate('/all');
+//     //     }
+//     // }, [navigate]);
+
+//     return (
+//         <div>
+//             <Routes>
+//                 <Route path='/all' element={<CategorialNews />} />
+//                 <Route path='/:title' element={<CategorialNews/>} />
+//                 <Route path='/:title/:index' element={<DetailedNews/>} />
+//                 <Route path='/news/' element={<Navigate to="/all" />} />
+//             </Routes>
+//         </div>
+//     );
+// }
+
+// export default Routing;
+
+
+import React, { useEffect } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import CategorialNews from '../Pages/CategorialNews';
 import DetailedNews from '../Pages/DetailedNews';
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
 
 const Routing = () => {
     const navigate = useNavigate();
@@ -19,10 +48,14 @@ const Routing = () => {
     return (
         <div>
             <Routes>
+                {/* Ana sayfaya giden yol */}
+                <Route path='/' element={<CategorialNews />} />
+                {/* Tüm haberler için yol */}
                 <Route path='/all' element={<CategorialNews />} />
-                <Route path='/:title' element={<CategorialNews />} />
+                {/* Detaylı haberler için yol */}
                 <Route path='/:title/:index' element={<DetailedNews />} />
-                <Route path='/news/' element={<Navigate to="/all" />} />
+                {/* Haberler için genel yol, burada Navigate yerine Redirect kullanılabilir */}
+                <Route path='/news/*' element={<Navigate to="/all" />} />
             </Routes>
         </div>
     );
