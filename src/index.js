25a82e26599f9c7f8a3fd7ac22df97ec9ae9da51
GunelHashimova <114391396+GunelHshmv@users.noreply.github.com>
2024-02-07 00:59:40 +0400

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter as Router } from 'react-router-dom';
-import { NewsProvider } from "./ContextData";
+import { NewsProvider } from "./ContextData/newsContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
