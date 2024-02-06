62406f725bd7e4a3beba6e1a3b5e8827e7b450f6
GunelHashimova <114391396+GunelHshmv@users.noreply.github.com>
2024-02-07 01:54:14 +0400

    setup gh-pages

--- src/NavLinks/index.js
+++ src/NavLinks/index.js
@@ -1,10 +1,10 @@
-import React, {  useContext, useState } from 'react';
+import React, {   useState } from 'react';
 import Customized from './Customized/Customized';
 import {Box, Button} from '@mui/material'
 import styles from '../Styles'
 
 const NavLinks = () => {
-    const [manual,setManual]=useState(['all', "business","sports","world","technology",'entertainment','science','national','hatke','politics',"automobile","miscellaneous","startup"])
+    const [manual]=useState(['all', "business","sports","world","technology",'entertainment','science','national','hatke','politics',"automobile","miscellaneous","startup"])
    return <Box style={styles.bgImageStyle}>
        <h1 style={styles.h1Styles}>News</h1>
             <Box style={styles.cotegoriesStyles}>
