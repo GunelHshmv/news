fb6db901ce5735a8b72dc91a7f1fcd87ff46f641
GunelHashimova <114391396+GunelHshmv@users.noreply.github.com>
2024-02-07 02:03:46 +0400

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { NewsProvider } from "./ContextData";
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <NewsProvider>
-      <Router>
-        <App />
-      </Router>
-    </NewsProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+      <NewsProvider>
+        <Router>
+          <App />
+        </Router>
+      </NewsProvider>
+  </React.StrictMode>
 );
 
-reportWebVitals(console.log);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
