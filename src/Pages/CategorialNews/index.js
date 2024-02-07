e912c2e9ecf35d833e079e51719a382d86970335
GunelHashimova <114391396+GunelHshmv@users.noreply.github.com>
2024-02-07 12:53:53 +0400

    setup gh-pages

--- src/Pages/CategorialNews/index.js
+++ src/Pages/CategorialNews/index.js
@@ -11,7 +11,7 @@ import { CardActionArea, CardActions } from '@mui/material';
 import Divider from '@mui/material/Divider';
 import { format, parse} from 'date-fns';
 import styles from '../../Styles'
-import newsContext, { NewsProvider } from '../../ContextData';
+import newsContext  from '../../ContextData';
 
 const CategorialNews = () => {
     const {title}=useParams()
