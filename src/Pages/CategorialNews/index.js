5d8f9212ac2fa37833752eba474ce42407e3e8d4
GunelHashimova <114391396+GunelHshmv@users.noreply.github.com>
2024-02-19 20:42:02 +0400

    setup gh-pages

--- src/Pages/CategorialNews/index.js
+++ src/Pages/CategorialNews/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import PageContainer from '../../PageContainer';
 import { Link, useParams } from 'react-router-dom';
 import Card from '@mui/material/Card';
